43380bd404747b48a551530c500b42119e993d2e
merkusvictory <mvhilario23@gmail.com>
2025-08-14 05:56:27 -0400

    fixed image rendering

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,54 @@
 import { useState, useEffect } from 'react'
 import ProjectCard from './ProjectCard';
 
+import gmail from '../assets/images/gmail.svg';
+import linkedin from '../assets/images/linkedin.svg';
+import github from '../assets/images/github.svg';
+
+import python from '../assets/images/python.svg';
+import java from '../assets/images/java.svg';
+import javascript from '../assets/images/javascript.svg';
+import cpp from '../assets/images/cpp.svg';
+import sql from '../assets/images/sql.svg';
+import reactLogo from '../assets/images/react.svg';
+import vite from '../assets/images/vite.svg';
+import node from '../assets/images/node.svg';
+import arduino from '../assets/images/arduino.svg';
+import postgresql from '../assets/images/postgresql.svg';
+import firebase from '../assets/images/firebase.svg';
+import supabase from '../assets/images/supabase.svg';
+import git from '../assets/images/git.svg';
+
+import blobtopiaThumbnail from '../assets/projects/blobtopia-thumbnail.png';
+import steampunchThumbnail from '../assets/projects/steampunch-thumbnail.png';
+import libraryThumbnail from '../assets/projects/library-thumbnail.png';
+import bookstoreimsThumbnail from '../assets/projects/bookstoreims-thumbnail.png';
+import heartledrThumbnail from '../assets/projects/heartledr-thumbnail.jpg';
+import vexroboticsThumbnail from '../assets/projects/vexrobotics-thumbnail.jpg';
+import awkwardcowThumbnail from '../assets/projects/awkwardcow-thumbnail.png';
+
 const Home = () => {
 
     const skills = [
-        { name: "Python", img: "src/assets/images/python.svg", color: "#e1ecda" },
-        { name: "Java", img: "src/assets/images/java.svg", color: "#e8dcd3" },
-        { name: "JavaScript", img: "src/assets/images/javascript.svg", color: "#fff4b3" },
-        { name: "C/C++", img: "src/assets/images/cpp.svg", color: "#cfe0f3" },
-        { name: "SQL", img: "src/assets/images/sql.svg", color: "#d2e4f5" },
-        { name: "React", img: "src/assets/images/react.svg", color: "#c9f1f6" },
-        { name: "Vite", img: "src/assets/images/vite.svg", color: "#e6d6fa" },
-        { name: "Node", img: "src/assets/images/node.svg", color: "#d4ead7" },
-        { name: "Arduino", img: "src/assets/images/arduino.svg", color: "#c4ebeb" },
-        { name: "PostgreSQL", img: "src/assets/images/postgresql.svg", color: "#d1dceb" },
-        { name: "Firebase", img: "src/assets/images/firebase.svg", color: "#ffeeb8" },
-        { name: "Supabase", img: "src/assets/images/supabase.svg", color: "#c6ebdc" },
-        { name: "Git", img: "src/assets/images/git.svg", color: "#f8d1c6" },
+        { name: "Python", img: python, color: "#f0e6ff" },
+        { name: "Java", img: java, color: "#fff3e0" },
+        { name: "JavaScript", img: javascript, color: "#fff9e6" },
+        { name: "C/C++", img: cpp, color: "#e6f0ff" },
+        { name: "SQL", img: sql, color: "#e6ffe6" },
+        { name: "React", img: reactLogo, color: "#e6f9ff" },
+        { name: "Vite", img: vite, color: "#f7e6ff" },
+        { name: "Node", img: node, color: "#e6ffe6" },
+        { name: "Arduino", img: arduino, color: "#e6f7ff" },
+        { name: "PostgreSQL", img: postgresql, color: "#e6ecff" },
+        { name: "Firebase", img: firebase, color: "#fff5e6" },
+        { name: "Supabase", img: supabase, color: "#e6fff2" },
+        { name: "Git", img: git, color: "#fff0e6" },
     ];
 
     const contacts = [
-        { name: "Email", img: "src/assets/images/gmail.svg", link: "mailto:mvhilario23@gmail.com", color: "#fde2d8" },
-        { name: "LinkedIn", img: "src/assets/images/linkedin.svg", link: "https://www.linkedin.com/in/marcus-hilario/", color: "#d1e3f4" },
-        { name: "Github", img: "src/assets/images/github.svg", link: "https://www.github.com/merkusvictory", color: "#e2e2e2" }
+        { name: "Email", img: gmail, link: "mailto:mvhilario23@gmail.com", color: "#fce8e6" }, // soft pastel red/pink
+        { name: "LinkedIn", img: linkedin, link: "https://www.linkedin.com/in/marcus-hilario/", color: "#e6f3f9" }, // soft pastel blue
+        { name: "Github", img: github, link: "https://www.github.com/merkusvictory", color: "#eaeaea" } // soft pastel gray
     ];
 
     return (
@@ -48,49 +74,49 @@ const Home = () => {
                     description="social media for blob-posting"
                     image="src/assets/projects/blobtopia-thumbnail.png"
                     color="#77dd77"
-                    link="/blobtopia"
+                    link={blobtopiaThumbnail}
                 />
                 <ProjectCard
                     title="SteamPunch"
                     description="motion-controlled platformer fighting game"
                     image="src/assets/projects/steampunch-thumbnail.png"
                     color="#B08968"
-                    link="/steampunch"
+                    link={steampunchThumbnail}
                 />
                 <ProjectCard
                     title="LIBRA-RY"
                     description="file management for law firms"
                     image="src/assets/projects/library-thumbnail.png"
                     color="#75befeff"
-                    link="/library"
+                    link={libraryThumbnail}
                 />
                 <ProjectCard
                     title="Online Bookstore IMS"
                     description="inventory management system for books"
                     image="src/assets/projects/bookstoreims-thumbnail.png"
                     color="#c1b379ff"
-                    link="/bookstore"
+                    link={bookstoreimsThumbnail}
                 />
                 <ProjectCard
                     title="Heart LEDR"
                     description="glowing no matter the distance"
                     image="src/assets/projects/heartledr-thumbnail.jpg"
                     color="#ff5d54ff"
-                    link="/heartledr"
+                    link={heartledrThumbnail}
                 />
                 <ProjectCard
                     title="VEX Robotics"
                     description="competition robots for world domination"
                     image="src/assets/projects/vexrobotics-thumbnail.jpg"
                     color="#acaca3ff"
-                    link="/vex"
+                    link={vexroboticsThumbnail}
                 />
                 <ProjectCard
                     title="Awkward Cow"
                     description="socially ineptly taking the milk back"
                     image="src/assets/projects/awkwardcow-thumbnail.png"
                     color="#ffc5d3"
-                    link="/awkwardcow"
+                    link={awkwardcowThumbnail}
                 />
             </div>
             <div className="home-divider">
