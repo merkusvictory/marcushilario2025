baa9fae64682b252f62d0b8660e50b5dbe74ab73
merkusvictory <mvhilario23@gmail.com>
2026-03-18 15:11:33 -0400

    added about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,18 @@
+import about_1 from "../assets/images/about_1.png"
+import about_2 from "../assets/images/about_2.png"
+import about_3 from "../assets/images/about_3.png"
+
 const About = () => {
     return (
         <div className="project-page about">
             <h1>ABOUT</h1>
-            <p>Under construction ...</p>
+            <p>I’m a New York based (and occasionally Philippines based) software developer currently pursuing my bachelor’s degree in Computer Science.</p>
+            <img draggable="false" src={about_1} style={{height: "300px", width: "auto"}} alt="about_1"/>
+            <p>Over the past three years I’ve been navigating the intersection of full-stack systems, robotics, and graphic design. I’ve helped build social media platforms, programmed world competition robots, and ran robotics state-leve competitions and university-level hackathons.</p>
+            <img draggable="false" src={about_2} alt="about_2"/>
+            <p>In my free time, I like to explore hidden study spaces around the city and build silly Discord bots.</p>
+            <img draggable="false" src={about_3} style={{height: "500px", width: "auto"}} alt="about_3"/>
+            <p>I’m always trying new things, so by the time you’re reading this there’s probably more to talk about, but thanks for stopping by!</p>
         </div>
     )
 }
